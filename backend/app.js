b9aa61c73b3c842b94ff3a0237133d688a306115
gangasandhu <gangathecoder@gmail.com>
2023-09-18 14:25:03 -0700

    error handling and other improvements

--- backend/app.js
+++ backend/app.js
@@ -19,11 +19,11 @@ app.get('/', (req, res) => {
 //   return maxId + 1
 // }
 
-app.post('/api/notes', (request, response) => {
-  const body = request.body
+app.post('/api/notes', (req, res) => {
+  const body = req.body
 
   if (!body.content) {
-    return response.status(400).json({
+    return res.status(400).json({
       error: 'content missing'
     })
   }
@@ -34,28 +34,71 @@ app.post('/api/notes', (request, response) => {
   })
 
   note.save().then(result => {
-    response.json(result)
+    res.json(result)
   })
 })
 
 app.get('/api/notes', (req, res) => {
-  Note.find({}).then(notes => {
-    res.json(notes)
-  })
+  Note.find({})
+    .then(notes => {
+      if (notes) {
+        res.json(notes)
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(500).end()
+    })
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
-
-  response.status(204).end()
+app.delete('/api/notes/:id', (req, res, next) => {
+  Note.findByIdAndRemove(req.params.id)
+    .then(result => {
+      res.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
-app.get('/api/notes/:id', (request, response) => {
-  Note.findById(request.params.id).then(note => {
-    response.json(note)
-  })  
+app.get('/api/notes/:id', (req, res, next) => {
+  Note.findById(req.params.id)
+    .then(note => {
+      if (note) {
+        res.json(note)
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
+
+app.put("/api/notes/:id", (req, res, next) => {
+  const body = req.body;
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+
+  Note.findByIdAndUpdate(req.params.id, note, {new: true})
+    .then(result => {
+      res.json(result)
+    })
+    .catch(error => next(error))
+})
+
+const errorHandler = (error, req, res, next) => {
+  console.log(error.message)
+
+  if(error.name === 'CastError') {
+    return res.status(404).send({error: 'malformatted id'})
+  }
+  
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
